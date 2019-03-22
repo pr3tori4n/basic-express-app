ed4553f49e6630e7a340cdeee656da0b66dc5b33
Harry Robbins <arrogantgopher@gmail.com>
2019-03-21 19:17:05 -0700

    added clean build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 const gulp = require('gulp');
+const del = require('del');
+
+gulp.task('clean', () => del(['./build/**', '!./build', '!./build/.gitkeep']));
 
 gulp.task('default', function(done) {
     console.log('I gulped!');
