3bfa5d1adf6bb42dac861a688438adef7befd975
Harry Robbins <arrogantgopher@gmail.com>
2019-03-21 19:47:53 -0700

    Add tasks and watcher for copying view files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
 const gulp = require('gulp');
 const del = require('del');
 
-gulp.task('clean', () => del(['./build/**', '!./build', '!./build/.gitkeep']));
+const clean = function() {
+    return del(['./build/**', '!./build', '!./build/.gitkeep']);
+};
 
-gulp.task('default', function(done) {
+//Copy pug files from components directory to express views directory
+const copyPugViewFiles_fromComponents_toExpressViewsFolder = function() {
+    return gulp.src('./components/**/*.pug')
+        .pipe(gulp.dest('./server/views/components'));
+};
+
+const watchFiles = function() {
+    //watch for changes in *.pug files under ./components, and copy them over automatically
+    gulp.watch('./components/**/*.pug', { ignoreInitial: false }, copyPugViewFiles_fromComponents_toExpressViewsFolder);
+};
+
+const log = function(done) {
     console.log('I gulped!');
     done();
-});
+};
+
+
+module.exports.clean = clean;
+module.exports.watch = watchFiles;
+module.exports.copyViews = copyPugViewFiles_fromComponents_toExpressViewsFolder;
+module.exports.default = gulp.series(clean, log);
