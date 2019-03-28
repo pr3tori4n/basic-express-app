4c8c803b153fe89870735f842a79fc75118f7062
Harry Robbins <arrogantgopher@gmail.com>
2019-03-27 18:43:57 -0700

    add a new gulp task for processing CSS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 const gulp = require('gulp');
-const del = require('del');
+const path = require('path');
+const del = require('del'); //Cleans out a directory of all folders and files
+//CSS tooling
+const sass = require('gulp-sass'); //gulp wrapper for node-sass to use with gulp streams
+sass.compiler = require('node-sass'); //Explicitly define the compiler used by gulp-sass instead of relying on default
+const plumber = require('gulp-plumber'); //Prevents crashes from watch if sass compiles fail or other node stream errors
+const postcss = require("gulp-postcss"); //Wrapper for autoprefixer and cleancss (postcss-clean)
+const autoprefixer = require("autoprefixer"); //Automatically adds vendor prefixes to appropriate css property names
+const cleancss = require('postcss-clean'); //Minifies CSS
+const rename = require("gulp-rename"); //for renaming a file in the stream
+
+const paths = {
+    css: {
+        src: path.join(__dirname, "src/client/**/*.scss"),
+        dest: path.join(__dirname, "build")
+    }
+};
 
 const clean = function() {
     return del(['./build/**', '!./build', '!./build/.gitkeep']);
@@ -16,6 +32,18 @@ const watchFiles = function() {
     gulp.watch('./src/components/**/*.pug', { ignoreInitial: false }, copyPugViewFiles_fromComponents_toExpressViewsFolder);
 };
 
+// CSS task
+const css = function() {
+return gulp.src("./src/client/**/*.scss", { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./build/"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(postcss([autoprefixer(), cleancss()]))
+    .pipe(gulp.dest("./build/", { sourcemaps: true }))
+    // .pipe(browsersync.stream());
+}
+
 const log = function(done) {
     console.log('I gulped!');
     done();
@@ -24,4 +52,5 @@ const log = function(done) {
 module.exports.clean = clean;
 module.exports.watch = watchFiles;
 module.exports.copyViews = copyPugViewFiles_fromComponents_toExpressViewsFolder;
-module.exports.default = gulp.series(clean, log);
+module.exports.css = css;
+module.exports.default = gulp.series(clean, css, log);
