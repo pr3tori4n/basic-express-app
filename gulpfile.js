a0b7530c5da349a5941fc94ea1e978e0371e467b
Harry Robbins <arrogantgopher@gmail.com>
2019-04-10 17:56:54 -0700

    add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,9 @@ const autoprefixer = require("autoprefixer"); //Automatically adds vendor prefix
 const cleancss = require('postcss-clean'); //Minifies CSS
 const rename = require("gulp-rename"); //for renaming a file in the stream
 //JS Tooling
-const replace = require('gulp-replace-path'); //for modifying paths in js files so imports work after moving the files using gulp.dest with different folder structure.
+const replace = require('gulp-replace-path'); //for modifying paths in js file so imports work after moving the files using gulp.dest with different folder structure.
+//Hot Reloading Tooling
+const browsersync = require("browser-sync").create();
 
 /* TODO: replace all paths with path.join for Cross-OS compatibility
 ** https://nodejs.org/api/path.html#path_path_join_paths
@@ -38,11 +40,17 @@ const copyImages = function() {
         .pipe(gulp.dest('./build/images'));
 };
 
-const watchFiles = function() {
-    //watch for changes in *.pug files under ./components, and copy them over automatically
-    gulp.watch('./src/components/**/*.pug', { ignoreInitial: false }, copyViews);
-    gulp.watch(['./src/client/**/*.js', './src/components/**/*.js'], { ignoreInitial: false }, js);
-    gulp.watch('./src/client/**/*.scss', css);
+const startBrowserSync = function(done) {
+    browsersync.init({
+        proxy: "http://localhost:8080", // port of node server
+        injectChanges: true
+    });
+    done();
+};
+
+const exitBrowserSync = function(done) {
+    browsersync.exit();
+    done();
 };
 
 // CSS task
@@ -54,23 +62,36 @@ const css = function() {
         .pipe(rename({ suffix: ".min" }))
         .pipe(postcss([autoprefixer(), cleancss()]))
         .pipe(gulp.dest("./build/", { sourcemaps: true }))
+        .pipe(browsersync.stream({match: '**/*.css'}));
 };
 // JS Tasks
 const js_application = function(done) {    
     return gulp.src("./src/client/**/*.js", { sourcemaps: true })
         .pipe(replace(/\.\.\/components/, './components')) //Replace components import directory because js_components() makes it a sibling directory
-        .pipe(gulp.dest("./build/", { sourcemaps: true }));
+        .pipe(gulp.dest("./build/", { sourcemaps: true }))
+        .pipe(browsersync.stream());
 };
 const js_components = function(done) {
     return gulp.src("./src/components/**/*.js", { sourcemaps: true })
-        .pipe(gulp.dest("./build/js/components", { sourcemaps: true }));
+        .pipe(gulp.dest("./build/js/components", { sourcemaps: true }))
+        .pipe(browsersync.stream());
 };
 const js = gulp.parallel(js_application, js_components);
+
+//File watching and auto-rebuild
+const watchFiles = function() {
+    //watch for changes in *.pug files under ./components, and copy them over automatically
+    gulp.watch('./src/components/**/*.pug', { ignoreInitial: false }, copyViews);
+    gulp.watch(['./src/client/**/*.js', './src/components/**/*.js'], js);
+    gulp.watch('./src/client/**/*.scss', css);
+};
 
 module.exports.clean = clean;
 module.exports.copyViews = copyViews;
 module.exports.copyImages = copyImages;
 module.exports.css = css;
 module.exports.js = js;
+module.exports.startBrowserSync = startBrowserSync;
+module.exports.exitBrowserSync = exitBrowserSync;
 module.exports.default = gulp.series(clean, gulp.parallel(copyViews, copyImages, css, js));
 module.exports.watch = watchFiles;
